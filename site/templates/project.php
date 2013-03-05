774521c3e52a838db3ce9c1f0e91601fb7881319
Matthew Daniels <hello@mattdanielsdesign.com>
2013-03-05 09:32:30 -0800

    Commented out Pintrest
    temporoary fix for extra footer space on project page

--- site/templates/project.php
+++ site/templates/project.php
@@ -99,6 +99,6 @@
     });
     </script>
 
-	<script type="text/javascript" src="//assets.pinterest.com/js/pinit.js"></script>        
+	<!-- <script type="text/javascript" src="//assets.pinterest.com/js/pinit.js"></script> -->        
 
 <?php snippet('footer') ?>
