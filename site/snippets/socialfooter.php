8502d7f2c51a07f359af9e3afe6130192221d368
Matthew Daniels <hello@mattdanielsdesign.com>
2013-05-08 01:21:13 -0700

    Fixed email address on homepage

--- site/snippets/socialfooter.php
+++ site/snippets/socialfooter.php
@@ -2,5 +2,5 @@
       <a href="https://twitter.com/MadeByCaravan" class="twitter">Twitter<span></span></a>
       <a href="http://www.facebook.com/WeAreCaravan" class="facebook">Facebook<span></span></a>
       <a href="http://vimeo.com/wearecaravan" class="vimeo">Vimeo<span></span></a>
-      <a href="mailto:hello@wearecaravan.com" class="email">Email<span></span></a>
+      <a href="mailto:hello@wearecaravan.tv" class="email">Email<span></span></a>
     </section>
